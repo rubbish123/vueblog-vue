7f4b4cb00b91e246651364ca293c23cf458df67b
rubbish123 <2358357084@qq.com>
2020-12-22 22:21:55 +0800

    使用插件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入element-ui依赖
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//引入axios
+import axios from "axios";
 
+Vue.use(Element)
+Vue.prototype.$axios=axios
 Vue.config.productionTip = false
 
 new Vue({
